bbb72a8ce8568abcdad2a6de05140da877479b0d
Aleksandr Popovich <alekpopo@pm.me>
2025-05-16 18:51:48 -0400

    add option to censor the username in logs
    Signed-off-by: Aleksandr Popovich <alekpopo@pm.me>

--- src/common/logging/backend.cpp
+++ src/common/logging/backend.cpp
@@ -4,6 +4,7 @@
 #include <atomic>
 #include <chrono>
 #include <climits>
+#include <regex>
 #include <thread>
 
 #include <fmt/ranges.h>
@@ -104,9 +105,23 @@ public:
             return;
         }
 
-        bytes_written += file->WriteString(FormatLogMessage(entry).append(1, '\n'));
+        auto message = FormatLogMessage(entry).append(1, '\n');
 
-               // Option to log each line rather than 4k buffers
+#ifndef ANDROID
+        if (Settings::values.censor_username.GetValue()) {
+            char* username = getenv("USER");
+            if (!username) {
+                username = getenv("USERNAME");
+            }
+
+            std::regex regex(username);
+            message = std::regex_replace(message, regex, "user");
+        }
+#endif
+
+        bytes_written += file->WriteString(message);
+
+        // Option to log each line rather than 4k buffers
         if (Settings::values.log_flush_lines.GetValue()) {
             file->Flush();
         }
